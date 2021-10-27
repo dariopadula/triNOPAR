caf884f4cb87a8a233efc9d55be7cba106ef5e58
dariopadula <dariopadula@gmail.com>
2021-10-27 10:02:34 -0300

    Agregue funciones iccFun y parInfoFun

---
+++ R/iccFun.R
@@ -0,0 +1,21 @@
+#' Para una grilla de puntos calcula la curva caracteristica del item ICC
+#'
+#'
+#'
+#' @param grilla valores de theta donde se evalua la funcion
+#' @param a parametro de discriminacion del la ICC
+#' @param b parametro de dificultad del la ICC
+#' @param c parametro de pseudo azar de la ICC
+#' @return Retorna un vector con la funcion evaluada en los puntos de la grilla
+#' @export
+#' @examples
+#' a = 1; b = 0; c = 0.1
+#' grilla = dnorm(seq(0,1,0.01))
+#' p = iccFun(a,b,c,grilla)
+
+
+
+iccFun = function(a,b,c,grilla) {
+  p = c + (1-c)/(1 + exp(-a*(grilla - b)))
+  return(p)
+}
---
+++ R/parInfoFun.R
@@ -0,0 +1,25 @@
+#' Para una grilla de puntos en el intervalo (0,1) calcula la funcion de informacion del item
+#'
+#'
+#'
+#' @param grilla valores de theta donde se evalua la funcion
+#' @param a parametro de discriminacion del la ICC
+#' @param b parametro de dificultad del la ICC
+#' @param c parametro de pseudo azar de la ICC
+#' @return Retorna un vector con la funcion de informacion evaluada en los puntos de la grilla
+#' @export
+#' @examples
+#' a = 1; b = 0; c = 0.1
+#' grillaUni = grilla =seq(0,1,0.01)
+#' infor = parInfoFun(a,b,c,grillaUni,trnfFun = qnorm)
+
+parInfoFun = function(a,b,c,grillaUni,trnfFun = qnorm) {
+### Pasa de la grilla en 0-1 a la distribucion de los thetas
+  grilla = eval(trnfFun(grillaUni))
+### Capcula p y q
+  p = iccFun(a,b,c,grilla)
+  q = 1 - p
+### Calcula la funcion de informacion
+  InfFun = (a^2)*((p - c)^2/(1 - c)^2)*(q/p)
+  return(InfFun)
+}
