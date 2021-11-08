2eb3e7b8e3109647d392c817ee203af0976baf29
mluzardov <mluzardov@gmail.com>
2021-11-08 11:15:39 -0300

    cambio prueba

--- Ejemplo1.R
+++ Ejemplo1.R
@@ -16,7 +16,9 @@ load_all()
 #Esto se debe inicializar una sola vez a menos que se quiera cambiar
 #el banco completo
 
+
 bancoITEMS<- genBancoDF(nparam=15)
+
 
 #Generamos un banco de 300 items
 set.seed(12345)
@@ -139,3 +141,6 @@ thetaiso=seq(0,1,0.001)
 nt=1000
 puntosicc=pnorm(ICCNPT$puntos)
 nucleod=epa
+
+
+thepl=qunif(300)
