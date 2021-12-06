4fae2ec389adb6e7b591d1e821523c712abb7993
CeciliaMarconi <cmarconi@ceibal.edu.uy>
2021-12-06 09:20:23 -0300

    subiendo

--- EjemploCecilia.R
+++ EjemploCecilia.R
@@ -331,7 +331,7 @@ res7=TAIgeneric(sujtai,
 
 rm(curvaNOPAR)
 
-
+bruncheando
 ###################################################
 #### Calculo de los errores
 errYses = ERRYSES(simData = res,grilla = seq(1:100)/100)
